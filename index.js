60187da87cbaa08d62d6cf2f717b417efebbdeac
Ishaqueahmed Mev <ishaqueahmed.mev@iflair.com>
2018-04-27 18:41:37 +0530

    Update parks with date and time

--- index.js
+++ index.js
@@ -12,12 +12,9 @@ var moment = require('moment')
 // SQL Connection
 var con = mysql.createConnection({
   host: "localhost",
-  // user: "dpwt",
-  // password: "2XJw$!G&Lsup5fG",
-  // database: "disneyparkswaitingtimes"
-  user: "root",
-  password: "",
-  database: "themepark"
+  user: "dpwt",
+  password: "2XJw$!G&Lsup5fG",
+  database: "disneyparkswaitingtimes"
 });
 
 con.connect(function (err) {
@@ -32,10 +29,7 @@ app.set('views', './views')
 app.set('view engine', 'pug');
 
 var something = function () {
-  // var executed = false;
-  // return function () {
-  //   if (!executed) {
-  console.log("1")
+  // console.log("1")
   var now = moment().format("YYYY/MM/DD")
   executed = true;
   // Update data
@@ -63,9 +57,6 @@ var something = function () {
             if (open[0].openingTime >= result[index].park_opening_time) {
               id = result[index].park_id
               if (new Date(open[0].openingTime).getTime() > new Date().getTime()) {
-                // console.log("IF :: " + index + " :: " + re.Name)
-                console.log("IF1 :: " + id + " " + open[0].openingTime)
-                console.log("IF2 :: " + id + " " + open[1].openingTime)
                 var up = "UPDATE tbl_park SET park_opening_time = '" + open[0].openingTime + "',park_closing_time = '" + open[0].closingTime + "', park_type = '" + open[0].type + "' WHERE park_name = '" + re.Name.replace("'", "") + "'";
                 con.query(up, function (err, result) {
                   if (err) throw err;
@@ -73,9 +64,6 @@ var something = function () {
                   io.sockets.emit('updatePark');
                 });
               } else {
-                console.log("ELSE1 :: " + id + " " + open[0].openingTime)
-                console.log("ELSE2 :: " + id + " " + open[1].openingTime)
-                // console.log("ELSE :: " + index + " :: " + re.Name)
                 var up = "UPDATE tbl_park SET park_opening_time = '" + open[1].openingTime + "',park_closing_time = '" + open[1].closingTime + "', park_type = '" + open[1].type + "' WHERE park_name = '" + re.Name.replace("'", "") + "'";
                 con.query(up, function (err, result) {
                   if (err) throw err;
@@ -84,7 +72,6 @@ var something = function () {
                 });
               }
             } else {
-              console.log("End of Time")
               io.sockets.emit('updatePark');
             }
           }
@@ -97,7 +84,6 @@ var something = function () {
             if (rideData.length > 0) {
               re.GetWaitTimes().then((rides) => {
                 for (var j = 0, ride; ride = rides[j++];) {
-                  // console.log("MY ID :: " + id)
                   var index = rideData.findIndex(x => x.ride_name.replace(/[^\w\s]/gi, '') == ride.name.replace(/[^\w\s]/gi, ''))
                   if (index < 0) {
                     var sql = "INSERT INTO tbl_ride(park_id,ride_name,ride_wait_time) VALUES ('" + id + "','" + ride.name.replace(/[^\w\s]/gi, '') + "','" + ride.waitTime + "')";
@@ -123,10 +109,9 @@ var something = function () {
               })
               // });
             } else {
-              // result.forEach(element => {
               re.GetWaitTimes().then((rides) => {
                 for (var j = 0, ride; ride = rides[j++];) {
-                  console.log(ride)
+                  // console.log(ride)
                   var sql = "INSERT INTO tbl_ride(park_id,ride_name,ride_wait_time) VALUES ('" + id + "','" + ride.name.replace(/[^\w\s]/gi, '') + "','" + ride.waitTime + "')";
                   con.query(sql, function (err, result) {
                     if (err) throw err;
@@ -178,73 +163,68 @@ app.get('/ride/:id', function (req, res) {
   })
 })
 
-var callParks = function () {
-  console.log("MATER")
-  // io.sockets.emit('updatePark');
-  con.query("SELECT * FROM tbl_park", function (err, result, fields) {
-    if (err) return console.log("1st :: " + err);
-    if (result.length > 0) {
-      con.query('SELECT * FROM tbl_ride', (err, rideData) => {
-        if (err) return err;
-        if (rideData.length > 0) {
-          result.forEach(element => {
-            var disneyMagicKingdom = new Themeparks.Parks[element.park_apiname]();
-            disneyMagicKingdom.GetWaitTimes().then((rides) => {
-              for (var j = 0, ride; ride = rides[j++];) {
-                var index = rideData.findIndex(x => x.ride_name.replace(/[^\w\s]/gi, '') == ride.name.replace(/[^\w\s]/gi, ''))
-                if (index < 0) {
-                  var sql = "INSERT INTO tbl_ride(park_id,ride_name,ride_wait_time) VALUES ('" + element.park_id + "','" + ride.name.replace(/[^\w\s]/gi, '') + "','" + ride.waitTime + "')";
-                  con.query(sql, function (err, result) {
-                    if (err) throw err;
-                    // console.log(result.affectedRows + " rides(s) added");
-                    io.sockets.emit('updatePark');
-                  });
-                } else {
-                  if (rideData[index].ride_wait_time != ride.waitTime) {
-                    var que = "UPDATE tbl_ride SET ride_wait_time = " + (isNaN(ride.waitTime) ? 0 : ride.waitTime) + " WHERE ride_id =" + rideData[index].ride_id;
-                    // console.log(que)
-                    con.query(que, function (err, rideResult) {
-                      if (err) throw err;
-                      // console.log(rideResult.affectedRows + " ride wait-time updated");
-                      io.sockets.emit('updateTime');
-                    });
-                  }
-                }
-              }
-            }, (err) => {
+// var callParks = function () {
+//   console.log("MATER")
+//   io.sockets.emit('updatePark');
+//   con.query("SELECT * FROM tbl_park", function (err, result, fields) {
+//     if (err) return console.log("1st :: " + err);
+//     if (result.length > 0) {
+//       con.query('SELECT * FROM tbl_ride', (err, rideData) => {
+//         if (err) return err;
+//         if (rideData.length > 0) {
+//           result.forEach(element => {
+//             var disneyMagicKingdom = new Themeparks.Parks[element.park_apiname]();
+//             disneyMagicKingdom.GetWaitTimes().then((rides) => {
+//               for (var j = 0, ride; ride = rides[j++];) {
+//                 var index = rideData.findIndex(x => x.ride_name.replace(/[^\w\s]/gi, '') == ride.name.replace(/[^\w\s]/gi, ''))
+//                 if (index < 0) {
+//                   var sql = "INSERT INTO tbl_ride(park_id,ride_name,ride_wait_time) VALUES ('" + element.park_id + "','" + ride.name.replace(/[^\w\s]/gi, '') + "','" + ride.waitTime + "')";
+//                   con.query(sql, function (err, result) {
+//                     if (err) throw err;
+//                     // console.log(result.affectedRows + " rides(s) added");
+//                     io.sockets.emit('updatePark');
+//                   });
+//                 } else {
+//                   if (rideData[index].ride_wait_time != ride.waitTime) {
+//                     var que = "UPDATE tbl_ride SET ride_wait_time = " + (isNaN(ride.waitTime) ? 0 : ride.waitTime) + " WHERE ride_id =" + rideData[index].ride_id;
+//                     // console.log(que)
+//                     con.query(que, function (err, rideResult) {
+//                       if (err) throw err;
+//                       // console.log(rideResult.affectedRows + " ride wait-time updated");
+//                       io.sockets.emit('updateTime');
+//                     });
+//                   }
+//                 }
+//               }
+//             }, (err) => {
 
-              // console.log("No ride to add")
-            })
-          });
-        } else {
-          result.forEach(element => {
-            var disneyMagicKingdom = new Themeparks.Parks[element.park_apiname]();
-            disneyMagicKingdom.GetWaitTimes().then((rides) => {
-              for (var j = 0, ride; ride = rides[j++];) {
-                var sql = "INSERT INTO tbl_ride(park_id,ride_name,ride_wait_time) VALUES ('" + element.park_id + "','" + ride.name.replace(/[^\w\s]/gi, '') + "','" + ride.waitTime + "')";
-                con.query(sql, function (err, result) {
-                  if (err) throw err;
-                  // console.log(result.affectedRows + " record(s) inserted");
-                });
-              }
-            }, (err) => {
-              // console.log("No ride found")
-            })
-          });
-        }
-      })
-    }
-    io.sockets.emit('updatePark');
-  });
-}
+//               // console.log("No ride to add")
+//             })
+//           });
+//         } else {
+//           result.forEach(element => {
+//             var disneyMagicKingdom = new Themeparks.Parks[element.park_apiname]();
+//             disneyMagicKingdom.GetWaitTimes().then((rides) => {
+//               for (var j = 0, ride; ride = rides[j++];) {
+//                 var sql = "INSERT INTO tbl_ride(park_id,ride_name,ride_wait_time) VALUES ('" + element.park_id + "','" + ride.name.replace(/[^\w\s]/gi, '') + "','" + ride.waitTime + "')";
+//                 con.query(sql, function (err, result) {
+//                   if (err) throw err;
+//                   // console.log(result.affectedRows + " record(s) inserted");
+//                 });
+//               }
+//             }, (err) => {
+//               // console.log("No ride found")
+//             })
+//           });
+//         }
+//       })
+//     }
+//     io.sockets.emit('updatePark');
+//   });
+// }
 
 something();
-// callParks();
 setInterval(something, 300000)
-// setInterval(function () {
-//   callParks();
-//   // io.sockets.emit('updatePark');
-// }, 100000);
 
 http.listen(3000, () => {
   console.log('Listening on *:3000')
